4f38fc7a52a554f5399311e7caa80d2757af695c
owlstechbd <owlstech.net@gmail.com>
2024-09-15 12:04:09 +0600

    chnagejgdf

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-window.onscroll = function() {
-  var navbar = document.querySelector('.nav-main');
-  if (window.pageYOffset > 50) { // You can adjust the scroll distance here
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-};
+document.addEventListener('DOMContentLoaded', function() {
+  window.onscroll = function() {
+    var navbar = document.querySelector('.nav-main');
+    if (window.pageYOffset > 50) { 
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  };
+});
+
+
+const mobileMenu = document.querySelector('#mobile-menu');
+const navList = document.querySelector('#nav-list');
+const closeMenu = document.querySelector('.close-button');
+
+mobileMenu.addEventListener('click', () => {
+  navList.classList.add('active');
+  closeMenu.classList.add('active'); // Show the close button
+});
+
+closeMenu.addEventListener('click', () => {
+  navList.classList.remove('active');
+  closeMenu.classList.remove('active'); // Hide the close button
+});
